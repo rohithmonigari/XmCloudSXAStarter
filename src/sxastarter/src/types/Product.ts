0dd6faa06260e31269d51a68626fe34dee07ec4e
Darvi Sunny <darvi.sunny@tadigital.com>
2023-05-24 22:10:56 +0530

    Removed Products

--- src/sxastarter/src/types/Product.ts
+++
@@ -1,9 +0,0 @@
-import { Field, ImageField } from '@sitecore-jss/sitecore-jss-nextjs';
-
-export type Product = {
-  fields: {
-    image: ImageField;
-    title: Field<string>;
-    price: Field<string>;
-  };
-};
